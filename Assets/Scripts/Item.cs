cbd645c3a4cf4b397eda9c635c64ff79ae8d7c01
Damian Dabrowski <dabdamian@gmail.com>
2021-04-24 19:25:05 +0200

    Created item script and variables.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //every item has a name
+    //every item has a description
+    //every item has an image icon
+    //every item has an attack strength
+
+        public string itemName = "Sword";
+        public string itemDescription = "gold, magic and powerful.";
+        public  string imageLink = "https://dabro.dev";
+        public int attackStrength  = 300;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The item  " + itemName + "and is " + itemDescription);
+        Debug.Log("Image link: " + imageLink);
+        Debug.Log("Strength: " + attackStrength);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
